3b2e12780532062e0d98c2bc937a7ea351acdf9a
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-04-01 10:43:50 +0530

    fixissues

--- LeaderBoard/src/Components/Header.jsx
+++ LeaderBoard/src/Components/Header.jsx
@@ -1,10 +1,15 @@
 import './Header.css'
 import king from "../assets/king.png" 
+import logo from "../assets/logo.png"
 const Header = () => {
     return <header>
        <div className='container'>
-        <img src={king} alt="" />
-        <h1>LeaderBoard </h1>
+        <div className='logo_wrapper'>
+            <img src={logo} className='appLogo'   alt="" />
+        </div>
+    <div className='wrapper'>    
+        <img src={king} className='leaderboard_logo' alt="" />
+        <h1>LeaderBoard </h1></div>
         
        </div>
     </header>
